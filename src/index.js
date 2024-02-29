f1a782704e8da78c259aba809ffb7adc944cbc97
unknown <jorge.martinez09@uptc.edu.co>
2024-02-29 16:06:38 -0500

    update to deploy

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/users_routers.js";
 
+const PORT = process.env.PORT || 3001; // Definir el puerto
+
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use('/api', userRoutes);
 
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () => {
+    console.log(`El servidor está corriendo en el puerto ${PORT}`);
+});
