985c61c01f14624b9a93375a11c4f7bbd036ff3a
unknown <jorge.martinez09@uptc.edu.co>
2024-02-29 16:09:05 -0500

    update port to deploy

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import userRolesRoutes from "./routes/roles_users_routes.js";
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use('/api', userRolesRoutes);
 
-app.listen(process.env.PORT||3000)
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+    console.log(`El servidor está corriendo en el puerto ${PORT}`);
+});
