44616eda2f38155a775c1097ad13cfbe688c50b3
unknown <jorge.martinez09@uptc.edu.co>
2024-02-29 21:27:38 -0500

    only deploy

--- src/index.js
+++ src/index.js
@@ -6,9 +6,20 @@ import typeDocumentsRoutes from "./routes/type_document_routes.js";
 import userRolesRoutes from "./routes/roles_users_routes.js";
 
 const app = express();
+const PORTS = [3000, 3001, 3002]; 
+
+const selectPort = () => {
+  for (const port of PORTS) {
+    if (!process.env.PORT || process.env.PORT == port) {
+      return port;
+    }
+  }
+  return PORTS[0]; 
+};
+
+const PORT = selectPort();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use('/api', userRoutes);
@@ -16,5 +27,6 @@ app.use('/api', rolesRoutes);
 app.use('/api', typeDocumentsRoutes);
 app.use('/api', userRolesRoutes);
 
-app.listen(3000)
-console.log('Server on port',3000)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
