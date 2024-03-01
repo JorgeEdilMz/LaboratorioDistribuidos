46b1bca9e00c7ac8ef51697869e84c2538a24008
unknown <jorge.martinez09@uptc.edu.co>
2024-02-29 21:33:58 -0500

    update ports

--- src/index.js
+++ src/index.js
@@ -6,18 +6,8 @@ import typeDocumentsRoutes from "./routes/type_document_routes.js";
 import userRolesRoutes from "./routes/roles_users_routes.js";
 
 const app = express();
-const PORTS = [3000, 3001, 3002]; 
-
-const selectPort = () => {
-  for (const port of PORTS) {
-    if (!process.env.PORT || process.env.PORT == port) {
-      return port;
-    }
-  }
-  return PORTS[0]; 
-};
-
-const PORT = selectPort();
+const PORT_START = 3000;
+let currentPort = PORT_START;
 
 app.use(cors());
 app.use(express.json());
@@ -27,6 +17,30 @@ app.use('/api', rolesRoutes);
 app.use('/api', typeDocumentsRoutes);
 app.use('/api', userRolesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+function startServer(port) {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+startServer(currentPort);
+currentPort++;
+
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
 });
+
+while (currentPort < PORT_START + 3) {
+  try {
+    startServer(currentPort);
+    break;
+  } catch (error) {
+    console.log(`Port ${currentPort} already in use, trying next port...`);
+    currentPort++;
+  }
+}
+
+if (currentPort === PORT_START + 3) {
+  console.error('All ports in use.');
+}
+
