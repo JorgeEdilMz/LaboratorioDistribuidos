2ae49a66838bf352c44556a29006d708ddfb6863
unknown <jorge.martinez09@uptc.edu.co>
2024-03-01 08:47:45 -0500

    add access all routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/users_routers.js";
+import rolesRoutes from "./routes/roles_routers_routes.js";
+import typeDocumentsRoutes from "./routes/type_document_routes.js";
+import userRolesRoutes from "./routes/roles_users_routes.js";
+
 
 const PORT = process.env.PORT || 3001; // Definir el puerto
 
@@ -10,6 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', userRoutes);
+app.use('/api', rolesRoutes);
+app.use('/api', typeDocumentsRoutes);
+app.use('/api', userRolesRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`El servidor está corriendo en el puerto ${PORT}`);
